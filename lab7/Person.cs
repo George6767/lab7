5afd10ba6bcb1892dc84c33e7fc7a72b921c9e9d
George6767 <129250696+George6767@users.noreply.github.com>
2023-06-22 07:03:14 +0300

    11:17 ok

---
+++ lab7/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class Person
+    {
+        static SqlConnection connection; 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Sun { get; set; }
+        static Person()
+        {
+            string strcon = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            connection = new SqlConnection(strcon);
+        }
+    }
+}
